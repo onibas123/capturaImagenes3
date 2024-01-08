72cdf7c34c494c32a97cb8bb47d74addf2da4d0d
onibas123 <el_mts@hotmail.com>
2024-01-08 16:54:55 -0300

    config

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,11 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
+	/*
 	'username' => 'root',
 	'password' => '',
 	'database' => 'capturaImagenes',
+	*/
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
