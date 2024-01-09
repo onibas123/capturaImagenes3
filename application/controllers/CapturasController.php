7cdfd2d150033502958b6e83da10ac40559a50d1
onibas123 <el_mts@hotmail.com>
2024-01-09 17:25:31 -0300

    Update CapturasController.php

--- application/controllers/CapturasController.php
+++ application/controllers/CapturasController.php
@@ -254,20 +254,15 @@ class CapturasController extends CI_Controller {
 		$pdf->AddPage('L', 'A4', 0);
 		$pdf->SetAutoPageBreak(true, 20);
 		//$y = $pdf->GetY();
-
+		/*
 		$pdf->SetFont('Times', '', 10);
 		$pdf->Cell(276, 10, 'Informe '.$date, 0, 0, 'C');
 		$pdf->Ln(20);
+		*/
 		//cabecera principal
 		// Logo
 		$pdf->Image(base_url().'assets/img/logo.png',10,6,30);
-		// Arial bold 15
-		$pdf->SetFont('Arial','B',15);
-		// Move to the right
-		$pdf->Cell(80);
-		// Title
-		$pdf->Cell(30,10,utf8_decode($nombre_empresa),1,0,'C');
-		// Line break
+		$this->Header($pdf);
 		$pdf->Ln(20);
 
 		$pdf->Cell(40, 10, utf8_decode($direccion_empresa), 1, 0, 'C');
@@ -296,5 +291,102 @@ class CapturasController extends CI_Controller {
 			}
 		}
 		$pdf->Output();	
+	}
+
+	public function Header($pdf){  
+		//Display Company Info
+		$pdf->SetFont('Arial','B',14);
+		$pdf->Cell(50,10,"ABC COMPUTERS",0,1);
+		$pdf->SetFont('Arial','',14);
+		$pdf->Cell(50,7,"West Street,",0,1);
+		$pdf->Cell(50,7,"Salem 636002.",0,1);
+		$pdf->Cell(50,7,"PH : 8778731770",0,1);
+		
+		//Display INVOICE text
+		$pdf->SetY(15);
+		$pdf->SetX(-40);
+		$pdf->SetFont('Arial','B',18);
+		$pdf->Cell(50,10,"INVOICE",0,1);
+		
+		//Display Horizontal line
+		$pdf->Line(0,48,210,48);
+	}
+	  
+	public function body($info,$products_info){
+		
+		//Billing Details
+		$this->SetY(55);
+		$this->SetX(10);
+		$this->SetFont('Arial','B',12);
+		$this->Cell(50,10,"Bill To: ",0,1);
+		$this->SetFont('Arial','',12);
+		$this->Cell(50,7,$info["customer"],0,1);
+		$this->Cell(50,7,$info["address"],0,1);
+		$this->Cell(50,7,$info["city"],0,1);
+		
+		//Display Invoice no
+		$this->SetY(55);
+		$this->SetX(-60);
+		$this->Cell(50,7,"Invoice No : ".$info["invoice_no"]);
+		
+		//Display Invoice date
+		$this->SetY(63);
+		$this->SetX(-60);
+		$this->Cell(50,7,"Invoice Date : ".$info["invoice_date"]);
+		
+		//Display Table headings
+		$this->SetY(95);
+		$this->SetX(10);
+		$this->SetFont('Arial','B',12);
+		$this->Cell(80,9,"DESCRIPTION",1,0);
+		$this->Cell(40,9,"PRICE",1,0,"C");
+		$this->Cell(30,9,"QTY",1,0,"C");
+		$this->Cell(40,9,"TOTAL",1,1,"C");
+		$this->SetFont('Arial','',12);
+		
+		//Display table product rows
+		foreach($products_info as $row){
+		  $this->Cell(80,9,$row["name"],"LR",0);
+		  $this->Cell(40,9,$row["price"],"R",0,"R");
+		  $this->Cell(30,9,$row["qty"],"R",0,"C");
+		  $this->Cell(40,9,$row["total"],"R",1,"R");
+		}
+		//Display table empty rows
+		for($i=0;$i<12-count($products_info);$i++)
+		{
+		  $this->Cell(80,9,"","LR",0);
+		  $this->Cell(40,9,"","R",0,"R");
+		  $this->Cell(30,9,"","R",0,"C");
+		  $this->Cell(40,9,"","R",1,"R");
+		}
+		//Display table total row
+		$this->SetFont('Arial','B',12);
+		$this->Cell(150,9,"TOTAL",1,0,"R");
+		$this->Cell(40,9,$info["total_amt"],1,1,"R");
+		
+		//Display amount in words
+		$this->SetY(225);
+		$this->SetX(10);
+		$this->SetFont('Arial','B',12);
+		$this->Cell(0,9,"Amount in Words ",0,1);
+		$this->SetFont('Arial','',12);
+		$this->Cell(0,9,$info["words"],0,1);
+		
+	}
+
+	public function Footer(){
+		
+		//set footer position
+		$this->SetY(-50);
+		$this->SetFont('Arial','B',12);
+		$this->Cell(0,10,"for ABC COMPUTERS",0,1,"R");
+		$this->Ln(15);
+		$this->SetFont('Arial','',12);
+		$this->Cell(0,10,"Authorized Signature",0,1,"R");
+		$this->SetFont('Arial','',10);
+		
+		//Display Footer Text
+		$this->Cell(0,10,"This is a computer generated invoice",0,1,"C");
+		
 	}
 }
