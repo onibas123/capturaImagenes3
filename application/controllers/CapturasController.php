a01c5cccf0d0762b857c172ebe6f81fa0acb2aad
onibas123 <el_mts@hotmail.com>
2024-01-09 10:37:10 -0300

    pdf

--- application/controllers/CapturasController.php
+++ application/controllers/CapturasController.php
@@ -206,4 +206,19 @@ class CapturasController extends CI_Controller {
 		$this->db->where('consolidado', 1);
 		echo json_encode($this->db->get()->result_array());
 	}
+
+	function ejemploPdf(){
+		// Get output html
+			$html = $this->output->get_output();
+			// Load pdf library
+			$this->load->library('pdf');
+			// Load HTML content
+			$this->pdf->loadHtml($html);
+			// (Optional) Setup the paper size and orientation
+			$this->pdf->setPaper('A4', 'landscape');
+			// Render the HTML as PDF
+			$this->pdf->render();
+			// Output the generated PDF (1 = download and 0 = preview)
+			$this->pdf->stream("welcome.pdf", array("Attachment"=>0));
+	   }
 }
