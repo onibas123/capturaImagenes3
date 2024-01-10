fdd8f386816a62a688de6e96f3cd44fca29e3bdf
onibas123 <el_mts@hotmail.com>
2024-01-10 12:57:33 -0300

    Update CapturasController.php

--- application/controllers/CapturasController.php
+++ application/controllers/CapturasController.php
@@ -319,7 +319,7 @@ class CapturasController extends CI_Controller {
 		$this->db->limit(1);
 		$organizacion = $this->db->get()->result_array();
 
-		$pdf->Line(10,50,200,50);
+		$pdf->Line(10,60,200,60);
 		$pdf->SetFont('Times', 'B', 10);
 		$pdf->Cell(40, 10, 'Rut: ', '', 0, 'L', false);
 		$pdf->SetFont('Times', 'U', 9);
@@ -347,7 +347,7 @@ class CapturasController extends CI_Controller {
 		$pdf->Cell(40, 10, utf8_decode('Teléfono Contacto: '), '', 0, 'L', false);
 		$pdf->SetFont('Times', 'U', 9);
 		$pdf->Cell(40, 10, (!empty($organizacion[0]['telefono']) ? $organizacion[0]['telefono'] : 'N/A'), '', 0, 'L', false);
-		$pdf->Line(10,70,200,70);
+		$pdf->Line(10,85,200,85);
 		$pdf->Ln();
 	}
 	  
