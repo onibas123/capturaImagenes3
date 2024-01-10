53f8c701989e2068423eab16556ae43011efc2a3
onibas123 <el_mts@hotmail.com>
2024-01-10 19:34:40 -0300

    aaa

--- application/controllers/CapturasController.php
+++ application/controllers/CapturasController.php
@@ -214,7 +214,7 @@ class CapturasController extends CI_Controller {
 		$dev = $this->input->get('dev', true);
 		$desde = $this->input->get('desde', true);
 		$hasta = $this->input->get('hasta', true);
-
+		
 		$this->db->select('capturas.ruta_imagen as imagen, dispositivos.ubicacion as ubicacion, capturas.canal as canal, capturas.observacion as observacion, DATE_FORMAT(capturas.fecha_hora, "%d-%m-%Y %H:%i") as fecha_hora ');
 		$this->db->from('capturas');
 		$this->db->join('dispositivos', 'dispositivos.id = capturas.dispositivos_id');
--- application/views/capturas/consolidado_pdf.php
+++ application/views/capturas/consolidado_pdf.php
@@ -55,14 +55,7 @@
 </style>
 </head>
 <body>
-<center>
-  <table class="table">
-    <tr>
-      <td><b><?php //echo $datos_empresa['nombre_empresa'];?></b> </td>
-  </tr>
 
-  <table>
-</center>
 <center>
 	<h3>
 		<?php
@@ -72,30 +65,7 @@
 	</h3>
 </center>
 <hr>
-<center>
-  <table class="table">
-    <tr>
-      <td><b>R.U.T:</b> </td>
-      <td><u><?php //echo $datos_organizacion[0]['rut'];?></u></td>
 
-      <td><b>Contacto:</b> </td>
-      <td><u><?php //echo $datos_organizacion[0]['contacto'];?></u></td>
-    </tr>
-
-    <tr>
-      <td><b>Razón Social:</b> </td>
-      <td colspan="3"><u><?php //echo $datos_organizacion[0]['razon_social'];?></u></td>
-    </tr>
-
-    <tr>
-      <td><b>Dirección:</b> </td>
-      <td><u><?php //echo $datos_organizacion[0]['direccion'];?></u></td>
-
-      <td><b>Tel Contacto:</b> </td>
-      <td><u><?php //echo $datos_organizacion[0]['telefono'];?></u></td>
-    </tr>
-  <table>
-</center>
 <table border="1" class="table">
   <thead>
     <tr>
