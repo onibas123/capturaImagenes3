4cc51c2af26ca7096163472f4fd9a05e722ebb25
onibas123 <el_mts@hotmail.com>
2024-02-04 01:19:04 -0300

    Update index.php

--- application/views/capturas/index.php
+++ application/views/capturas/index.php
@@ -31,9 +31,4 @@ foreach($css_files as $file): ?>
     if($('#ruta_imagen_display_as_box').length > 0){
         $('#ruta_imagen_display_as_box').html('Imagen');
     }
-
-    if($('#field-usuario_id').length > 0){
-        if($('#field-usuario_id').html().trim() == '')
-            $('#field-usuario_id').html('Sistema');
-    }
 </script>
