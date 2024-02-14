4e9fe463584eb0e5f44ee46a71934eb9d469ad6f
onibas123 <el_mts@hotmail.com>
2024-02-13 23:53:30 -0300

    Update CapturasController.php

--- application/controllers/CapturasController.php
+++ application/controllers/CapturasController.php
@@ -338,7 +338,7 @@ class CapturasController extends CI_Controller {
 		$this->load->library('M_pdf');
 		$this->m_pdf->pdf->WriteHTML($html);
 		$filename = $org."_".$dev."_".date('YmdHis').".pdf";
-		$this->m_pdf->pdf->Output($filename, './assets/reportes/');
+		$this->m_pdf->pdf->Output($filename, base_url().'/assets/reportes/');
 		echo $filename;
 	}
 
